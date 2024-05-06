2eb8a518a8d906bf0140f42dbdbddbcb1c1177da
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-05-06 15:48:28 +0100

    Update train_trasfert_learning.py

--- PyTorch_YOWO-main/train_trasfert_learning.py
+++ PyTorch_YOWO-main/train_trasfert_learning.py
@@ -320,7 +320,7 @@ def train():
                 for k in loss_dict_reduced.keys():
                     log += '[{}: {:.2f}]'.format(k, loss_dict[k])
                 l_dictio=list(loss_dict_reduced.keys())
-                wandb.log({"loss_conf train":l_dictio[0] ,"loss_cls train":l_dictio[1] ,"loss_box train ":l_dictio[2] ,"losses train ":l_dictio[3] })
+                wandb.log({"loss_conf train":float(l_dictio[0]) ,"loss_cls train":float(l_dictio[1]) ,"loss_box train ":float(l_dictio[2]) ,"losses train ": float(l_dictio[3]) })
                 # other infor
                 log += '[time: {:.2f}]'.format(t1 - t0)
                 log += '[size: {}]'.format(d_cfg['train_size'])
